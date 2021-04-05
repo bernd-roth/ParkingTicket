8797653c351d3df9e568aef28050a563f1d57cfd
bernd-roth <berndroth0@gmail.com>
2021-04-05 15:10:01 +0200

    * introducing new different static variables

--- app/src/main/java/at/co/netconsulting/parkingticket/statics/StaticVariables.java
+++ app/src/main/java/at/co/netconsulting/parkingticket/statics/StaticVariables.java
@@ -6,4 +6,9 @@ public class StaticVariables {
     public static final String BUTTON_60 = "60";
     public static final String BUTTON_90 = "90";
     public static final String BUTTON_120 = "120";
+    public static final String BUTTON_START_BOOKING = "Start booking";
+    public static final String KEY = "KEY";
+    public static final String VALUE = "VALUE";
+    public static final int JOB_ID = 1;
+    public static final int PERMISSION_ALL = 1;
 }
