2fa49298dd94d10a7cdfd8fc6e631686b23cc759
bernd-roth <berndroth0@gmail.com>
2021-04-06 22:21:38 +0200

    * not needed anymore

--- app/src/main/java/at/co/netconsulting/parkingticket/BroadcastReceiver/SMSBroadcastReceiver.java
+++
@@ -1,5 +0,0 @@
-package at.co.netconsulting.parkingticket.BroadcastReceiver;
-
-public class SMSBroadcastReceiver {
-    public static final String ACTION_SMS_BROADCAST_RECEIVER = "at.co.netconsulting.intent.action.ACTION_SMS_BROADCAST_RECEIVER";
-}
