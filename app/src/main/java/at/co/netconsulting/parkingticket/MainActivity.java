b70ca5c8506d670d2bd5c75abc83ee950b4b349e
bernd-roth <berndroth0@gmail.com>
2021-04-06 19:06:35 +0200

    * ResultReceiver for showing AlertDialog coming from JobIntentService * stopping JobIntentService

--- app/src/main/java/at/co/netconsulting/parkingticket/MainActivity.java
+++ app/src/main/java/at/co/netconsulting/parkingticket/MainActivity.java
@@ -1,22 +1,27 @@
 package at.co.netconsulting.parkingticket;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
 import androidx.core.app.ActivityCompat;
-
 import at.co.netconsulting.parkingticket.CalculateBookings.CalculateBookings;
 import at.co.netconsulting.parkingticket.general.BaseActivity;
 import at.co.netconsulting.parkingticket.service.IntentServiceManager;
-
+import at.co.netconsulting.parkingticket.statics.StaticVariables;
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
 import static at.co.netconsulting.parkingticket.statics.StaticVariables.*;
 
 public class MainActivity extends BaseActivity {
@@ -29,7 +34,9 @@ public class MainActivity extends BaseActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.SEND_SMS};
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.FOREGROUND_SERVICE};
+    TreemapResultReceiver treeMapResultReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,16 +66,17 @@ public class MainActivity extends BaseActivity {
         timerIntervall.setValue(0);
 
         calculateBookings = new CalculateBookings();
+        intentServiceManager = new Intent(this, IntentServiceManager.class);
     }
 
     public void startTasks() {
-        intentServiceManager = new Intent(this, IntentServiceManager.class);
+        treeMapResultReceiver = new TreemapResultReceiver(new Handler(Looper.getMainLooper()));
+        intentServiceManager.putExtra("receiver", treeMapResultReceiver);
         IntentServiceManager.enqueueWork(this, intentServiceManager);
         startForegroundService(intentServiceManager);
     }
 
     //Permissions handling
-
     public void askPermission() {
         for (String permission : PERMISSIONS) {
             if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
@@ -82,15 +90,47 @@ public class MainActivity extends BaseActivity {
         switch (requestCode) {
             case 1: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                    //Do your work.
                 } else {
-                    Toast.makeText(this, getResources().getString(R.string.permission_denied), Toast.LENGTH_SHORT).show();
+                    makeText(this, getResources().getString(R.string.permission_denied), LENGTH_SHORT).show();
                 }
             }
         }
     }
 
-    //
+    private class TreemapResultReceiver extends ResultReceiver {
+        public TreemapResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            switch (resultCode) {
+                case BOOKING_ERROR:
+                    stopService(intentServiceManager);
+                    showAlertDialog();
+                    makeText(getApplicationContext(), "Error in booking parking ticket. Please leave a gap when entering your first ticket and current time",
+                            Toast.LENGTH_LONG).show();
+                    break;
+            }
+            super.onReceiveResult(resultCode, resultData);
+        }
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage("ERROR: Please book tickets with a time gap between current time and first ticket of at least 2 minutes!")
+                .setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("Alert message");
+        alert.show();
+    }
+
     public void control_handler(View view) {
         Button whichControlWasClicked = (Button)view;
         int tpHour = clockStartTimer.getHour();
