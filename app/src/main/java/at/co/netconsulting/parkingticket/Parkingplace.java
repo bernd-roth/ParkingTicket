5c17cc6656cfae6ebdcc7877312dbc1103153675
Bernd Roth <berndroth0@gmail.com>
2022-07-22 23:25:38 +0200

    * Parkingplaces extended with Leaflet * Anrainerparken extended with Leaflet * AlertDialog with RadioButton to choose which parkingzones to show

--- app/src/main/java/at/co/netconsulting/parkingticket/Parkingplace.java
+++ app/src/main/java/at/co/netconsulting/parkingticket/Parkingplace.java
@@ -1,6 +1,8 @@
 package at.co.netconsulting.parkingticket;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -12,9 +14,14 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.util.ArrayList;
+
 import at.co.netconsulting.parkingticket.general.BaseActivity;
 
 public class Parkingplace extends BaseActivity implements GeolocationPermissions.Callback {
+
+    private ArrayList mSelectedItems = new ArrayList();
+    private int choiceParking;
 
     @JavascriptInterface
     @Override
@@ -22,36 +29,94 @@ public class Parkingplace extends BaseActivity implements GeolocationPermissions
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parkingpplaces);
 
-        final LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-        boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        showAlertDialog();
 
-        ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            final WebView browser;
-            browser=(WebView)findViewById(R.id.webView);
+    }
 
-            final WebSettings webSettings = browser.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-            webSettings.setAppCacheEnabled(true);
-            webSettings.setDatabaseEnabled(true);
-            webSettings.setDomStorageEnabled(true);
-            webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-            webSettings.setGeolocationEnabled(true);
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Parkingplace.this);
+        alertDialog.setTitle("AlertDialog");
+        String[] items = {"Kurzparkzone Wien", "Anrainerparken Wien"};
+        int checkedItem = 1;
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                final LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+                boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-            GeoClient geo = new GeoClient();
-            browser.setWebChromeClient(geo);
-            String origin = "";
-            geo.onGeolocationPermissionsShowPrompt(origin, this);
-            browser.getSettings().setJavaScriptEnabled(true);
+                ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            browser.setWebViewClient(new WebViewClient() {
-                @Override
-                public void onPageFinished(WebView view, String url) {
-                    String messagea, messageb, messagec, messaged, messagee, messagef, messageg = null;
-                    browser.loadUrl("javascript:AndroidFunction(" + null + "," + null + "," + null + "," + null + "," + null + "," + null + "," + null + "," + 12 + ")");
+                final WebView browser;
+                browser=(WebView)findViewById(R.id.webView);
+
+                final WebSettings webSettings = browser.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setAppCacheEnabled(true);
+                webSettings.setDatabaseEnabled(true);
+                webSettings.setDomStorageEnabled(true);
+                webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+                webSettings.setGeolocationEnabled(true);
+
+                GeoClient geo = new GeoClient();
+                browser.setWebChromeClient(geo);
+                String origin = "";
+                geo.onGeolocationPermissionsShowPrompt(origin, Parkingplace.this);
+                browser.getSettings().setJavaScriptEnabled(true);
+
+                browser.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        browser.loadUrl("javascript:init(" + choiceParking + "," + "12)");
                     }
                 });
-            browser.loadUrl("file:///android_asset/index.html");
+                browser.loadUrl("file:///android_asset/index.html");
+            }
+        });
+        alertDialog.setMultiChoiceItems(R.array.geojson, null,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if (isChecked) {
+                            if(which==0) {
+                                mSelectedItems.add(1);
+                            } else if(which==1) {
+                                mSelectedItems.add(2);
+                            }
+                        }
+                        else if (mSelectedItems.contains(0)) {
+                            mSelectedItems.remove(Integer.valueOf(1));
+                        } else if(mSelectedItems.contains(1)) {
+                            mSelectedItems.remove(Integer.valueOf(2));
+                        }
+                    }
+                })
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK, so save the mSelectedItems results somewhere
+                        // or return them to the component that opened the dialog
+                        if(mSelectedItems.size()==0) {
+                            choiceParking = -1;
+                        } else if(mSelectedItems.size()==1) {
+                            if(mSelectedItems.contains(1)) {
+                                choiceParking = 1;
+                            } else if(mSelectedItems.contains(2)) {
+                                choiceParking = 2;
+                            }
+                        } else if(mSelectedItems.size()==2) {
+                            choiceParking = 3;
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+        AlertDialog alert = alertDialog.create();
+        alert.setCanceledOnTouchOutside(false);
+        alert.show();
     }
 
     @Override
@@ -65,4 +130,5 @@ public class Parkingplace extends BaseActivity implements GeolocationPermissions
             callback.invoke(origin, true, false);
         }
     }
+
 }
